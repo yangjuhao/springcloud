8c2046656d4264c7f540a4590dd73b2bf612e8df
杨聚浩 <1079242928@qq.com>
2022-06-06 17:16:01 +0800

    修改配置

--- config-client/src/main/java/com/yang/springcloud/config/controller/ConfigClientController.java
+++ config-client/src/main/java/com/yang/springcloud/config/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.yang.springcloud.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RefreshScope // 监控刷新
 public class ConfigClientController {
 
     @Value("${config.info}")
